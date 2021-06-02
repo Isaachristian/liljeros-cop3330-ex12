2c8c656a0d202521323812ad611016c3b473f025
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 20:11:08 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variables
+        double principle, rate, years, fianlValue;
+
+        System.out.print("Enter the principle: ");
+        principle = scanner.nextDouble();
+        System.out.print("Enter the rate of interest: ");
+        rate = scanner.nextDouble();
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextDouble();
+
+        fianlValue = principle * (1 + (rate / 100) * years);
+
+        System.out.println("After " + (int)years + " years at " + rate + "%, the investment will be worth $" + String.format("%.2f", fianlValue));
+
+    }
+}
